a2968cd5d58aa8fdeb275b0c6dff04d6fd91c2e4
Prashant Kattimani -X (prkattim - TATA CONSULTANCY SERVICES LIMITED at Cisco) <prkattim@partnet.cisco.com>
2020-03-05 17:36:35 +0530

    removed django-heroku module and related line

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,5 +125,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
-
-django_heroku.settings(locals())
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
