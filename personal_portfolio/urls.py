938d131817bca88fb69f37ab34d4e30d6545eb85
Prashant Kattimani -X (prkattim - TATA CONSULTANCY SERVICES LIMITED at Cisco) <prkattim@partnet.cisco.com>
2020-03-09 20:25:48 +0530

    added root path for application

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from projects import views
 
 urlpatterns = [
+    path('', views.project_index, name='project_index'),
     path('admin/', admin.site.urls),
     # path('', include('hello_world.urls'))
     path('projects/', include('projects.urls')),
