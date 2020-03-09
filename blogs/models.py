f288a955a5fc3a05aeb46139c46ad210d3dd1925
Prashant Kattimani -X (prkattim - TATA CONSULTANCY SERVICES LIMITED at Cisco) <prkattim@partnet.cisco.com>
2020-03-09 20:16:00 +0530

    Added unique condition for categories

--- blogs/models.py
+++ blogs/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-	name = models.CharField(max_length=20)
+	name = models.CharField(max_length=20, unique=True)
 
 	def __str__(self):
 		return self.name
